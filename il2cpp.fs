08f93ed307d615e937a7d6c9eef2c4e11e3f44b9
NULL Field <78525180+unreliablecode@users.noreply.github.com>
2023-08-08 15:35:51 +0700

    Update il2cpp.fs

--- il2cpp.fs
+++ il2cpp.fs
@@ -8,23 +8,22 @@ open BepInEx.Unity.IL2CPP
 [<BepInPlugin("unreliablecode.com", "MyPlugin", "1.0.0")>]
 type MainClass() =
     inherit BepInEx.Unity.IL2CPP.BasePlugin()
-
+    
     override this.Load() =
+        
         let newObject = GameObject("MyGameObject")
         Object.DontDestroyOnLoad(newObject)
         newObject.hideFlags <- HideFlags.HideAndDontSave
         ClassInjector.RegisterTypeInIl2Cpp<MyMonoBehaviour>()
         let myMonoBehaviour = newObject.AddComponent<MyMonoBehaviour>()
         myMonoBehaviour.Initialize()
-
-        UnityEngine.Debug.Log("Plugin loaded, GameObject created, and MyMonoBehaviour added!")
+        printfn("Mod Loaded") //Changed From Debug.Log since idk how to cast string to il2cpp object in FSharp
 
 and MyMonoBehaviour() =
     class
         inherit UnityEngine.MonoBehaviour()
-        let mutable TimeDown = 5f
         member this.Initialize() =
-            UnityEngine.Debug.Log("MyMonoBehaviour initialized!")
+            printfn("MyMonoBehaviour initialized!") //Changed From Debug.Log since idk how to cast string to il2cpp object in FSharp
 
         member this.Start() =
             this.NormalStart()
@@ -33,9 +32,7 @@ and MyMonoBehaviour() =
             this.NormalUpdate()
 
         member private this.NormalStart() =
-            UnityEngine.Debug.Log("MyMonoBehaviour Start")
+            printfn("MyMonoBehaviour Start") //Changed From Debug.Log since idk how to cast string to il2cpp object in FSharp
         member private this.NormalUpdate() =
-            TimeDown <- TimeDown - Time.deltaTime
-            if TimeDown < 0.0f then
-                UnityEngine.Debug.Log("MyMonoBehaviour Update")
+                printfn("MyMonoBehaviour Update") //Changed From Debug.Log since idk how to cast string to il2cpp object in FSharp
     end
