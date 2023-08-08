e5d140eae41d5fd2da8f2b21ec12c81d3a769e6f
NULL Field <78525180+unreliablecode@users.noreply.github.com>
2023-08-08 14:59:58 +0700

    Update il2cpp.fs
    added some example to Register Class in IL2CPP and to Create MonoBehaviour Class

--- il2cpp.fs
+++ il2cpp.fs
@@ -1,7 +1,9 @@
 namespace MyBepInPlugin
 
 open BepInEx
+open Il2CppInterop.Runtime.Injection
 open UnityEngine
+open BepInEx.Unity.IL2CPP
 
 [<BepInPlugin("unreliablecode.com", "MyPlugin", "1.0.0")>]
 type MainClass() =
@@ -11,5 +13,29 @@ type MainClass() =
         let newObject = GameObject("MyGameObject")
         Object.DontDestroyOnLoad(newObject)
         newObject.hideFlags <- HideFlags.HideAndDontSave
+        ClassInjector.RegisterTypeInIl2Cpp<MyMonoBehaviour>()
+        let myMonoBehaviour = newObject.AddComponent<MyMonoBehaviour>()
+        myMonoBehaviour.Initialize()
 
-        UnityEngine.Debug.Log("Plugin loaded and GameObject created!")
+        UnityEngine.Debug.Log("Plugin loaded, GameObject created, and MyMonoBehaviour added!")
+
+and MyMonoBehaviour() =
+    class
+        inherit UnityEngine.MonoBehaviour()
+        let mutable TimeDown = 5f
+        member this.Initialize() =
+            UnityEngine.Debug.Log("MyMonoBehaviour initialized!")
+
+        member this.Start() =
+            this.NormalStart()
+
+        member this.Update() =
+            this.NormalUpdate()
+
+        member private this.NormalStart() =
+            UnityEngine.Debug.Log("MyMonoBehaviour Start")
+        member private this.NormalUpdate() =
+            TimeDown <- TimeDown - Time.deltaTime
+            if TimeDown < 0.0f then
+                UnityEngine.Debug.Log("MyMonoBehaviour Update")
+    end
